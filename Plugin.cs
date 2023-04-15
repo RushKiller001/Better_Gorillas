e640a0101045513e469952ec831f4a4751eccdce
Graze <graze.the.sergal@gmail.com>
2023-04-15 20:38:19 +0100

    Improved Performance and Removed Transparent Ice

--- Plugin.cs
+++ Plugin.cs
@@ -21,20 +21,20 @@ namespace Better_Gorillas
     {
         public Material material;
         public Material material2;
+        AssetBundle bundle;
         void Start()
         {
+            Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream("Better_Gorillas.Mat.mat");
+            bundle = AssetBundle.LoadFromStream(s);
             Utilla.Events.GameInitialized += OnGameInitialized;
         }
 
         void OnGameInitialized(object sender, EventArgs e)
         {
             HarmonyPatches.ApplyHarmonyPatches();
-            Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream("Better_Gorillas.Mat.rushspinmat");
-            AssetBundle bundle = AssetBundle.LoadFromStream(s);
-            Stream sT = Assembly.GetExecutingAssembly().GetManifestResourceStream("Better_Gorillas.Mat.betterice");
-            AssetBundle bundle2 = AssetBundle.LoadFromStream(sT);
+            FindObjectOfType<VRRig>().gameObject.AddComponent<Matchanger>();
             material = bundle.LoadAsset<Material>("Animated");
-            material2 = bundle2.LoadAsset<Material>("BetterIce");
+            material2 = bundle.LoadAsset<Material>("BetterIce");
         }
     }
     public class Matchanger : MonoBehaviour
